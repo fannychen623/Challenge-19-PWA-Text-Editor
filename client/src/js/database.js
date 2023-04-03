18ea5f6231883e3dc1a81cd2af46fdc5ae233344
fycn <fannychen623@gmail.com>
2023-04-03 10:50:42 -0400

    fixed local storage and indexdb load

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content)  => {
-  console.log('Post to the database');
+  console.log('Update the database');
 
   // Create a connection to the database database and version we want to use.
   const jateDb = await openDB('jate', 1);
@@ -25,8 +25,9 @@ export const putDb = async (content)  => {
   // Open up the desired object store.
   const store = tx.objectStore('jate');
 
-  // Use the .add() method on the store and pass in the content.
-  const request = store.add({ content: content });
+  const clearStore = store.clear();
+
+  const request = store.add({content});
 
   // Get confirmation of the request.
   const result = await request;
@@ -51,8 +52,8 @@ export const getDb = async () => {
 
   // Get confirmation of the request.
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  console.log('result.value', result[0].content);
+  return result[0].content;
 };
 
 initdb();
